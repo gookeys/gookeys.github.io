1047ffe9f62c860ec884d0dca26d32ec18f23c55
iamsgokul <iamsgokul@gmail.com>
2021-04-30 20:06:42 +0530

    Store Update

--- store/js/menu.js
+++ store/js/menu.js
@@ -1,9 +1,7 @@
 $(function () {
     $('#navbar').load('nav.html');
     $('#footerbar').load('footer.html');
-})
 
-$(document).ready(function () {
     $('.menu').click(function () {
         $('header ul').toggleClass('open');
     })
